ffe9f461d4ea6103d9c93f19ab71e3645b1b7638
yoann-dufresne <yoann.dufresne0@gmail.com>
2021-08-28 14:57:03 +0200

    WIP

--- gateway_pjon.ino
+++ gateway_pjon.ino
@@ -1,18 +1,20 @@
+/* serial define*/
 #define TIMEOUT 5000
 #define CONNECTION_ID 42
 
+/* serial global variable*/
 bool connected = false;
 long last_ping = 0;
 char ledPin = LED_BUILTIN;
 
-void setup() {
+/* serial section */
+void serial_setup() {
   Serial.begin(115200);
-
   pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin, LOW);  
 }
 
-void loop() {
+void serial_loop() {
   if (!connected) {
     connection();
   } 
@@ -21,10 +23,9 @@ void loop() {
       connected = false;
 
     if (Serial.available())
-      readInput();
+      read_serial();
   }
 }
-
 
 void connection () {
   Serial.flush();
@@ -56,24 +57,56 @@ void connection () {
 }
 
 
-void readInput(){
+uint8_t read_buffer[255] = {0};
+void read_serial(){
   String str = "";
-  String ping = "PING ?";
+  
+  size_t message_length = Serial.read();
+  Serial.readBytes(read_buffer, message_length);
 
-  str = Serial.readStringUntil('\n');
-
-  if (ping.equals(str))
-    pong();
+  if(pong(read_buffer, message_length)) {}
+  else if(serial2pjon(read_buffer, message_length)) {}
 }
 
-void pong () {
-  String pong = "PONG !";
+bool pong(uint8_t* message, uint8_t length) {
+  if(length == 6) {
+    if(message[0] == 'P' &&
+       message[1] == 'I' &&
+       message[2] == 'N' &&
+       message[3] == 'G' &&
+       message[4] == ' ' &&
+       message[5] == '?') {
+        String pong = "PONG !";
+        connected = true;
 
-  connected = true;
+        if (connected) {
+          Serial.println(pong);
+          last_ping = millis();
+        }
 
-  if (connected) {
-    Serial.println(pong);
-    last_ping = millis();
+        return true;
+      } 
   }
 
+  return false;
+}
+
+bool serial2pjon(uint8_t* message, uint8_t length) {
+  if(connected) {
+    Serial.println(length);
+    Serial.write(message, length);
+    Serial.println(" ok");
+
+    return true;
+  }
+
+  return false;
+}
+
+void setup() {
+  serial_setup();
+}
+
+void loop() {
+  serial_loop();
 }

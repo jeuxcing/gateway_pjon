5624243fc17bb0a2380af419b33f384640058098
Yoann Dufresne <yoann.dufresne0@gmail.com>
2021-08-28 18:30:27 +0200

    Network pin update

--- gateway_pjon.ino
+++ gateway_pjon.ino
@@ -20,6 +20,13 @@ void serial_setup() {
   digitalWrite(ledPin, LOW);
 }
 
+
+void wire_receiver(uint8_t *payload, uint16_t length, const PJON_Packet_Info &info) {
+  Serial.write((uint8_t)length);
+  Serial.write(payload, length);
+  Serial.flush();
+}
+
 void pjon_setup() {
   main_bus.strategy.set_pin(NET_PIN);
   main_bus.set_id('M');
@@ -106,10 +113,46 @@ bool pong(uint8_t* message, uint8_t length) {
   return false;
 }
 
-bool serial2pjon(uint8_t* message, uint8_t length) {
+uint8_t buffer[50];
+
+bool serial2pjon(uint8_t* message, uint8_t length) {  
   if(connected) {
-    main_bus.send_packet_blocking(message[0], message, length);
-    return true;
+    uint16_t result;
+    if (message[0] != 'L' and message[0] != 'S')
+      result = main_bus.send_packet_blocking(message[0], message, length);
+    else if (message[0] == 'L') {
+      // command
+      buffer[0] = message[0];
+      // Line
+      buffer[1] = message[2];
+      // Led
+      uint8_t segment_size = message[1] == 'R' ? 12 : 24;
+      buffer[2] = message[3] * segment_size + message[4];
+      // Colors
+      buffer[3] = message[5];
+      buffer[4] = message[6];
+      buffer[5] = message[7];
+      
+      result = main_bus.send_packet_blocking(message[1], buffer, length);
+    } else if (message[0] == 'S') {
+      // command
+      buffer[0] = message[0];
+      // Line
+      buffer[1] = message[2];
+      // Led start
+      uint8_t segment_size = message[1] == 'R' ? 12 : 24;
+      buffer[2] = message[3] * segment_size + message[4];
+      // Led stop
+      buffer[3] = message[3] * segment_size + message[5];
+      // Colors
+      buffer[4] = message[6];
+      buffer[5] = message[7];
+      buffer[6] = message[8];
+      
+      result = main_bus.send_packet_blocking(message[1], buffer, length);
+    }
+    
+    return result;
   }
 
   return false;
@@ -119,6 +162,49 @@ void setup() {
   serial_setup();
   pjon_setup();
 }
+
+
+void debug() {
+  uint8_t msg[10];
+  msg[0] = 'L';
+  uint8_t red[] = {0, 100, 0};
+  uint8_t black[] = {0, 0, 0};
+  
+  for (int l=0 ; l<3 ; l++) {
+    msg[1] = l;
+    
+    for (int led=0 ; led<36 ; led++) {
+      msg[2] = led;
+      memcpy((msg+3), red, 3);
+      uint16_t result = main_bus.send_packet_blocking('R', msg, 6);
+      Serial.println(result);
+      delay(50);
+      memcpy((msg+3), black, 3);
+      main_bus.send_packet_blocking('R', msg, 6);
+    }
+
+    for (int led=0 ; led<48 ; led++) {
+      msg[2] = led;
+      memcpy((msg+3), red, 3);
+      uint16_t result = main_bus.send_packet_blocking('H', msg, 6);
+      Serial.println(result);
+      delay(50);
+      memcpy((msg+3), black, 3);
+      main_bus.send_packet_blocking('H', msg, 6);
+    }
+
+    for (int led=0 ; led<48 ; led++) {
+      msg[2] = led;
+      memcpy((msg+3), red, 3);
+      uint16_t result = main_bus.send_packet_blocking('V', msg, 6);
+      Serial.println(result);
+      delay(50);
+      memcpy((msg+3), black, 3);
+      main_bus.send_packet_blocking('V', msg, 6);
+    }
+  }
+}
+
 
 void loop() {
   serial_loop();
